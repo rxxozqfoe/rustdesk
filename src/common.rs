bbf6f7bcb90d5ee40c09f9e37f419483d0d29c64
jimmy <jimmy@mail.jsdv.info>
2026-04-11 12:14:35 +0800

    change my custom key for custom.txt

--- src/common.rs
+++ src/common.rs
@@ -2183,7 +2183,7 @@ pub fn read_custom_client(config: &str) {
         log::error!("Failed to decode custom client config");
         return;
     };
-    const KEY: &str = "5Qbwsde3unUcJBtrx9ZkvUmwFNoExHzpryHuPUdqlWM=";
+    const KEY: &str = "fjMWQpn+Kvu2hO6hRjIWyS8n55JITpevt0OzMBRIn4Q=";
     let Some(pk) = get_rs_pk(KEY) else {
         log::error!("Failed to parse public key of custom client");
         return;
